1aadd83dc4dfdb16d888f8941386131fa1f7477a
Yanik <user@MacBook-Pro.local>
2024-02-01 00:14:37 +0300

    make the ht_6 program, but I don't know about some way for more fast alg

--- HT_6.py
+++ HT_6.py
@@ -1,12 +1,42 @@
-A = 265
-n = 9
+import matplotlib
+
+A = 0  # the user number
+n = 0  # the sqrt 
+
+while 1:
+    A = int(input("Введите число: "))
+    n = int(input("Введите степень корня: "))
+
+    if n % 2 == 0 and A < 0: 
+        print("У отрицательного числа не может быть чётного корня. Введите корректные данные.")
+    elif n == 0:
+        print("Степень корня не может быть равна нулю. Введите корректные данные.")
+    else:
+        break
 
 x = 2
+x1 = 0
+
+crutch = 0
+if n < 0:  
+    crutch = n  # crutch for n < 0 
+    n = n * -1
 
 while x < A:
-    x1 = 1/n * ((n-1) * x + (A / (x**(n-1))))
-    print(round(x1, 4))
-    if round(x, 3) == round(x1, 3):
+    if n == 1:
+        print(f"Арифметическим корнем степени {n} числа {A} является само число {A} ")  # exception with sqrt = 1
         break
+    if A == 0:
+        print(f"Любой корень числа {A} равен самому {A} ")  # exception for 0
+        break
+
+    x1 = 1/n * ((n-1) * x + (A / (x**(n-1))))
+    if round(x, 3) == round(x1, 3) and crutch < 0:  # crutch!!! (костыли - наше всё!)
+        print(f"Арифметическим корнем степени {n} числа {A} является {1/(round(x1, 4))} ")
+        break
+    elif round(x, 3) == round(x1, 3):
+        print(f"Арифметическим корнем степени {n} числа {A} является {round(x1, 4)} ")  # show the answer
+        break
+
     else:
         x = x1
